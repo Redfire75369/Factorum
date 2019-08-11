a27e557f153445af19f589b0ee9dac0ff0e0df27
TheSilkMiner <sir.karati@outlook.com>
2019-08-11 13:44:54 +0200

    Add more options for the sorter script
    Signed-off-by: TheSilkMiner <sir.karati@outlook.com>

--- etc/SilkSorter.java
+++ etc/SilkSorter.java
@@ -1,15 +1,75 @@
 
 public final class SilkSorter implements Runnable {
     
-    private static final int SILK_SORTER_INVALID = 0x0;
-    private static final int SILK_SORTER_BEGIN = 0x1;
-    private static final int SILK_SORTER_END = 0x2;
+    private enum SorterMarkerFlags {
+        INVALID, BEGIN, END;
+    }
+    
+    private interface Sorter {
+        int sort(/*@NotNull*/ final String a, /*@NotNull*/ final String b);
+    }
+    
+    private interface Transformer {
+        /*@NotNull*/ String transform(/*@NotNull*/ final String string, final boolean wasSorted);
+    }
+    
+    static class DefaultSorter implements Sorter {
+        @Override
+        public int sort(/*@NotNull*/ final String a, /*@NotNull*/ final String b) {
+            return a.trim().toLowerCase(java.util.Locale.ENGLISH).compareTo(b.trim().toLowerCase(java.util.Locale.ENGLISH));
+        }
+    }
+    
+    static class QuotedStringSorter implements Sorter {
+        private static final Sorter DEFAULT = new DefaultSorter();
+        @Override
+        public int sort(/*@NotNull*/ final String a, /*@NotNull*/ final String b) {
+            final String trimmedA = a.trim().substring(1, a.trim().length() - 2);
+            final String trimmedB = b.trim().substring(1, b.trim().length() - 2);
+            return DEFAULT.sort(trimmedA, trimmedB);
+        }
+    }
+    
+    static class NewLineTransfomer implements Transformer {
+        @Override
+        public /*@NotNull*/ String transform(/*@NotNull*/ final String string, final boolean wasSorted) {
+            return string + "\n";
+        }
+    }
+    
+    static class CommaArrayTransformer implements Transformer {
+        private boolean isFirst = true;
+        private boolean wasPrevSorted = false;
+        // |3€[[@(_)$€ |>]-[(_)[[]]< ``//*(_)
+        @Override
+        public /*@NotNull*/ String transform(/*@NotNull*/ final String string, final boolean wasSorted) {
+            if (this.isFirst) {
+                this.isFirst = false;
+                return string;
+            }
+            final String noCommaString = string.replace(",", "");
+            final String booleanTransformPass;
+            if (this.wasPrevSorted && wasSorted) {
+                booleanTransformPass = ",\n" + noCommaString;
+            } else if (this.wasPrevSorted && !wasSorted) {
+                booleanTransformPass = "\n" + noCommaString;
+            } else if (!this.wasPrevSorted && wasSorted) {
+                booleanTransformPass = "\n" + noCommaString;
+            } else {
+                booleanTransformPass = "\n" + string;
+            }
+            this.wasPrevSorted = wasSorted;
+            return booleanTransformPass;
+        }
+    }
     
     /*@NotNull*/ /*@NonNls*/ private static final String SILK_SORTER_COMMENT = "//SILK_SORTING: ";
     /*@NotNull*/ /*@NonNls*/ private static final String SILK_SORTER_COMMENT_BEGIN = "//SILK_SORTING: BEGIN@";
     /*@NotNull*/ /*@NonNls*/ private static final String SILK_SORTER_COMMENT_END = "//SILK_SORTING: END@";
     
     /*@NotNull*/ private final java.util.List<java.io.File> filesToActOn = new java.util.ArrayList<>();
+    /*@Nullable*/ private /*mutable*/ Sorter sorter = null;
+    /*@NotNull*/ private final java.util.List<Transformer> transformers = new java.util.ArrayList<>();
     private /*mutable*/ boolean dryRun = false;
     
     private SilkSorter() {}
@@ -24,6 +84,14 @@ public final class SilkSorter implements Runnable {
     
     private void dryRun() {
         this.dryRun = true;
+    }
+    
+    private void sorter(/*@NotNull*/ final Sorter sorter) {
+        this.sorter = sorter;
+    }
+    
+    private void addTransformer(/*@NotNull*/ final Transformer transformer) {
+        this.transformers.add(transformer);
     }
     
     @Override
@@ -41,27 +109,46 @@ public final class SilkSorter implements Runnable {
             }
         }
         
+        class LineHolder {
+            /*@NotNull*/ final String string;
+            final boolean sorted;
+            
+            LineHolder(/*@NotNull*/ final String s, final boolean t) {
+                this.string = s;
+                this.sorted = t;
+            }
+        }
+        
+        class TransformerHolder {
+            /*@NotNull*/ /*mutable*/ String s;
+        }
+        
         if (!file.exists()) {
             System.out.println("File " + file + " does not exist; skipping");
             return;
         }
         
+        if (this.sorter == null) {
+            System.out.println("Null sorter: replacing with default");
+            this.sorter = new DefaultSorter();
+        }
+        
         System.out.println("Beginning operation on file " + file.toPath());
         
         /*@Nullable*/ /*mutable*/ SectionHolder holder = null;
-        final java.util.List<String> fileLines = new java.util.ArrayList<String>();
+        final java.util.List<LineHolder> fileLines = new java.util.ArrayList<>();
         
         try (final java.io.BufferedReader reader = java.nio.file.Files.newBufferedReader(file.toPath(), java.nio.charset.StandardCharsets.UTF_8)) {
             /*mutable*/ String line = null;
             while ((line = reader.readLine()) != null) {
-                final int silkSorterMarker = this.trySilkSorterMarker(line);
-                if (silkSorterMarker == SILK_SORTER_INVALID) {
+                final SorterMarkerFlags silkSorterMarker = this.trySilkSorterMarker(line);
+                if (silkSorterMarker == SorterMarkerFlags.INVALID) {
                     if (holder != null) {
                         holder.swallowedLines.add(line);
                     } else {
-                        fileLines.add(line);
+                        fileLines.add(new LineHolder(line, false));
                     }
-                } else if (silkSorterMarker == SILK_SORTER_BEGIN) {
+                } else if (silkSorterMarker == SorterMarkerFlags.BEGIN) {
                     if (holder != null) {
                         System.err.println("Unable to start a section when already in a section. Current section: " + holder.sectionName);
                         System.err.println("Unable to continue; aborting processing of file");
@@ -72,9 +159,9 @@ public final class SilkSorter implements Runnable {
                         System.err.println("Malformed section name; aborting processing of file");
                         return;
                     }
-                    fileLines.add(line.trim());
+                    fileLines.add(new LineHolder(line.trim(), false));
                     holder = new SectionHolder(sectionName);
-                } else if (silkSorterMarker == SILK_SORTER_END) {
+                } else if (silkSorterMarker == SorterMarkerFlags.END) {
                     if (holder == null) {
                         System.err.println("Unable to end a section when it hasn't been started.");
                         System.err.println("Cannot continue: processing aborted");
@@ -92,8 +179,8 @@ public final class SilkSorter implements Runnable {
                     final java.util.List<String> swallowedLines = new java.util.ArrayList<String>(holder.swallowedLines);
                     holder = null;
                     this.sortSwallowedLines(swallowedLines);
-                    swallowedLines.forEach(fileLines::add);
-                    fileLines.add(line.trim());
+                    swallowedLines.forEach(it -> fileLines.add(new LineHolder(it, true)));
+                    fileLines.add(new LineHolder(line.trim(), false));
                 } else {
                     throw new IllegalStateException("" + silkSorterMarker);
                 }
@@ -112,13 +199,19 @@ public final class SilkSorter implements Runnable {
         
         try (final java.io.BufferedWriter writer = java.nio.file.Files.newBufferedWriter(outputFile.toPath(), java.nio.charset.StandardCharsets.UTF_8, java.nio.file.StandardOpenOption.WRITE, java.nio.file.StandardOpenOption.CREATE)) {
             fileLines.forEach(it -> {
+                final TransformerHolder transfHolder = new TransformerHolder();
+                transfHolder.s = it.string;
+                this.transformers.forEach(transformer -> {
+                    final String transformed = transformer.transform(transfHolder.s, it.sorted);
+                    transfHolder.s = transformed;
+                });
                 try {
-                    writer.write(it);
-                    writer.write('\n');
+                    writer.write(transfHolder.s);
                 } catch (final java.io.IOException e) {
                     throw new RuntimeException(e);
                 }
             });
+            writer.write("\n");
             writer.flush();
         } catch (final java.io.IOException e) {
             System.err.println("The following error occurred while processing the current file. Aborting");
@@ -128,25 +221,25 @@ public final class SilkSorter implements Runnable {
         System.out.println("Processing completed successfully");
     }
     
-    private int trySilkSorterMarker(/*@NotNull*/ final String line) {
+    private SorterMarkerFlags trySilkSorterMarker(/*@NotNull*/ final String line) {
         final String realLine = line.trim();
-        if (!realLine.startsWith(SILK_SORTER_COMMENT)) return SILK_SORTER_INVALID;
-        if (realLine.startsWith(SILK_SORTER_COMMENT_BEGIN)) return SILK_SORTER_BEGIN;
-        if (realLine.startsWith(SILK_SORTER_COMMENT_END)) return SILK_SORTER_END;
+        if (!realLine.startsWith(SILK_SORTER_COMMENT)) return SorterMarkerFlags.INVALID;
+        if (realLine.startsWith(SILK_SORTER_COMMENT_BEGIN)) return SorterMarkerFlags.BEGIN;
+        if (realLine.startsWith(SILK_SORTER_COMMENT_END)) return SorterMarkerFlags.END;
         throw new IllegalArgumentException("Wrong SILK_SORTER_COMMENT");
     }
     
     /*@Nullable*/
-    private String trySectionName(final int marker, /*@NotNull*/ final String line) {
-        if (marker == SILK_SORTER_INVALID) throw new IllegalArgumentException(line);
-        if (marker == SILK_SORTER_BEGIN) {
+    private String trySectionName(final SorterMarkerFlags marker, /*@NotNull*/ final String line) {
+        if (marker == SorterMarkerFlags.INVALID) throw new IllegalArgumentException(line);
+        if (marker == SorterMarkerFlags.BEGIN) {
             final String sect = line.substring(SILK_SORTER_COMMENT_BEGIN.length());
             if (sect.length() <= 0) {
                 return null;
             }
             return sect;
         }
-        if (marker == SILK_SORTER_END) {
+        if (marker == SorterMarkerFlags.END) {
             final String sect = line.substring(SILK_SORTER_COMMENT_END.length());
             if (sect.length() <= 0) {
                 return null;
@@ -160,10 +253,11 @@ public final class SilkSorter implements Runnable {
         java.util.Collections.sort(lines, (c, d) -> {
             final String a = (String) c;
             final String b = (String) d;
-            return a.trim().toLowerCase(java.util.Locale.ENGLISH).compareTo(b.trim().toLowerCase(java.util.Locale.ENGLISH));
+            return this.sorter.sort(a, b);
         });
     }
     
+    @SuppressWarnings("unchecked")
     public static void main(/*@NotNull*/ final String... args) {
         final SilkSorter sorter = new SilkSorter();
         if (args.length <= 0) {
@@ -178,6 +272,26 @@ public final class SilkSorter implements Runnable {
                 }
                 if ("--help".equals(arg)) {
                     throw new RuntimeException("Not yet");
+                }
+                if (arg.startsWith("--sorter=")) {
+                    try {
+                        final String className = arg.substring("--sorter=".length());
+                        final Class<? extends Sorter> sorterClass = (Class<? extends Sorter>) Class.forName(className);
+                        sorter.sorter((Sorter) sorterClass.newInstance());
+                        continue;
+                    } catch (final ReflectiveOperationException e) {
+                        throw new RuntimeException(arg, e);
+                    }
+                }
+                if (arg.startsWith("--transformer=")) {
+                    try {
+                        final String className = arg.substring("--transformer=".length());
+                        final Class<? extends Transformer> transformerClass = (Class<? extends Transformer>) Class.forName(className);
+                        sorter.addTransformer((Transformer) transformerClass.newInstance());
+                        continue;
+                    } catch (final ReflectiveOperationException e) {
+                        throw new RuntimeException(arg, e);
+                    }
                 }
                 System.err.println("Illegal command " + arg);
                 throw new IllegalArgumentException();
